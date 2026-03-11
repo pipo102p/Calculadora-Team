d8a1c4415005aeda5e64a04694526025c1f2599d
Diurno <Diurno@DAM126>
2026-03-11 10:15:51 +0100

    Commit inicial: Suma y Resta

--- EJEVALUABLE/src/calculadora/Calculadora.java
+++ EJEVALUABLE/src/calculadora/Calculadora.java
@@ -1,11 +1,17 @@
 package calculadora;
 
 public class Calculadora {
-	 public double sumar(double a, double b) { return a + b; }
-	 public double restar(double a, double b) { return a - b; }
-	 public static void main(String[] args) {
-	 Calculadora calc = new Calculadora();
-	 System.out.println("--- INICIO DE LA CALCULADORA ---");
-	 System.out.println("Suma: " + calc.sumar(10, 5));
-	 }
+	public double sumar(double a, double b) {
+		return a + b;
 	}
+
+	public double restar(double a, double b) {
+		return a - b;
+	}
+
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		System.out.println("--- INICIO DE LA CALCULADORA ---");
+		System.out.println("Suma: " + calc.sumar(10, 5));
+	}
+}

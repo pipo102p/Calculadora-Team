da10743bcbdc2d6f1acc78eaf8354647b1102582
Diurno <Diurno@DAM112>
2026-03-18 08:55:06 +0100

    division

--- EJEVALUABLE/src/calculadora/Calculadora.java
+++ EJEVALUABLE/src/calculadora/Calculadora.java
@@ -14,4 +14,5 @@ public class Calculadora {
 		System.out.println("--- INICIO DE LA CALCULADORA ---");
 		System.out.println("Suma: " + calc.sumar(10, 5));
 	}
+	public double dividir(double a, double b) { return a / b; }
 }
